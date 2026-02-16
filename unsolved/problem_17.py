4f1025be85f2fd4893af2edbe94bfd7626f3998e
dlambda <20195teel@gmail.com>
2026-02-16 10:52:26 -0500

    finished 17

--- unsolved/problem_17.py
+++ unsolved/problem_17.py
@@ -1,29 +1,66 @@
 #!/usr/bin/env python3
 
 
-def read_data(filename):
-    pyramid = []
-    
-    with open(filename, 'r') as f:
-        data = f.readlines()
-    
-    for line in range(len(data)):
-        str_nums = line.split()
-        for str_num in str_nums:
-            int_num = int(str_num)
-            
-            pyramid[line].append(int_num)
-            
-    return pyramid
+# the words lol
+SINGLE = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+]
+DOUBLE = [
+    "",  # empty for 0 and 1
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+]
 
-def brute_force_paths(pyramid):
-    for row in range(len(pyramid) - 1):
-        for col in range(len(pyramid[row])):
-            
-            
+
+def num_to_english(num):
+    if 0 <= num < 20:
+        return SINGLE[num]
+    if 20 <= num < 100:
+        return DOUBLE[num // 10] + (SINGLE[num % 10] if (num % 10 != 0) else "")
+    if 100 <= num < 1000:
+        return SINGLE[num // 100] + "hundred" + (("and" + num_to_english(num % 100)) if (num % 100 != 0) else "")
+    if num == 1000:
+        return "onethousand"
 
 
-if __name__ == '__main__':
-    pyramid = read_data('data/problem_18.txt')
-    brute_force_paths(pyramid)
-    print(max(pyramid[-1]))
+def sum_of_numwords(max):
+    return sum(len(num_to_english(i)) for i in range(1, max + 1))
+
+
+def test_translation():
+    if num_to_english(15) != "fifteen":
+        raise "FAILED 15"
+    if num_to_english(78) != "seventyeight":
+        raise "FAILED 78"
+    if num_to_english(575) != "fivehundredandseventyfive":
+        raise "FAILED 575"
+
+
+if __name__ == "__main__":
+    print(sum_of_numwords(1000))
